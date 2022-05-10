7f38ad3b5dc81867d84fe83eaf84587ca9400e0c
Odinlevi <odinlevi@gmail.com>
2022-05-10 16:35:07 +0300

    added template

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.use('/static', express.static(__dirname + "/static"));
+app.set("view engine", "ejs");
+
 var MongoClient = require("mongodb").MongoClient;
 MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true},
     function (error, client) {
@@ -9,6 +12,10 @@ MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true},
 
     app.get("/", function(req, res) {
        res.send("Hello world");
+    });
+
+    app.get("/admin/dashboard", function (req, res) {
+        res.render("admin/dashboard");
     });
 
     app.listen(3000, function () {
